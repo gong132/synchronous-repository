60cea7604c8ed00db37712d967c8122d13ff7bac
Echarts <130134210@qq.com>
2020-09-10 20:36:13 +0800

    fix: 修改了lintstyle检测规则

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,16 +1,13 @@
-const fabric = require('@umijs/fabric');
-
 module.exports = {
-    ...fabric.stylelint,
     "extends": ["stylelint-config-standard", "stylelint-config-prettier"],
     "rules": {
-      "no-descending-specificity": null,
-      "selector-pseudo-class-no-unknown": [
-        true,
-        {
-          "ignorePseudoClasses": ["global"]
-        }
-      ],
-      "selector-pseudo-element-colon-notation": "single"
+        "no-descending-specificity": null,
+        "selector-pseudo-class-no-unknown": [
+            true,
+            {
+                "ignorePseudoClasses": ["global"]
+            }
+        ],
+        "selector-pseudo-element-colon-notation": "single"
     }
 };
