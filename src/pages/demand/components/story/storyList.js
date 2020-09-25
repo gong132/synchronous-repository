efd7a829a2bd06e8fc30c2f8d7fd53c14d181c8f
Echarts <130134210@qq.com>
2020-09-25 09:25:13 +0800

    feat: 需求详情story操作相关

--- src/pages/demand/components/story/storyList.js
+++ src/pages/demand/components/story/storyList.js
@@ -1,83 +1,237 @@
-import React, {useEffect, useState} from "react";
-import styles from "@/pages/demand/index.less";
-import StandardTable from "@/components/StandardTable";
-import AddStory from "@/pages/demand/components/story/addStory";
-import ITAssess from "@/pages/demand/components/story/ITAssess";
-import TurnAssess from "@/pages/demand/components/story/turnAssign";
-import {PagerHelper, TableColumnHelper} from "@/utils/helper";
-import { connect } from "dva";
-import {Button, Col, DatePicker, Form, Icon, Input, Popover, Row, Select} from "antd";
-import { formLayoutItem, formLayoutItem2 } from "@/utils/constant";
-import {STORY_PRIORITY, STORY_STATUS, STORY_TYPE} from "@/pages/demand/util/constant";
-import classNames from "classnames";
-import bottomIcon from "@/assets/icon/drop_down.svg";
-import upIcon from "@/assets/icon/Pull_up.svg";
+import React, { Fragment, useEffect, useState } from 'react';
+import styles from '@/pages/demand/index.less';
+import StandardTable from '@/components/StandardTable';
+import AddStory from '@/pages/demand/components/story/addStory';
+import ITAssess from '@/pages/demand/components/story/ITAssess';
+import TurnAssess from '@/pages/demand/components/story/turnAssign';
+import { PagerHelper, TableColumnHelper } from '@/utils/helper';
+import { connect } from 'dva';
+import {
+  Button,
+  Col,
+  DatePicker,
+  Divider,
+  Form,
+  Icon,
+  Input,
+  message,
+  Popconfirm,
+  Popover,
+  Row,
+  Select,
+  Tooltip,
+} from 'antd';
+import { formLayoutItem, formLayoutItem2 } from '@/utils/constant';
+import { STORY_PRIORITY, STORY_STATUS, STORY_TYPE } from '@/pages/demand/util/constant';
+import classNames from 'classnames';
+import bottomIcon from '@/assets/icon/drop_down.svg';
+import upIcon from '@/assets/icon/Pull_up.svg';
+import { isEmpty } from '@/utils/lang';
+import storage from '@/utils/storage';
+import OptButton from '@/components/commonUseModule/optButton';
+import edit from '@/assets/icon/Button_bj.svg';
+import { router } from 'umi/index';
+import deleteIcon from '@/assets/icon/Button_del.svg';
 
 const FormItem = Form.Item;
 const { Option } = Select;
 const { RangePicker } = DatePicker;
 const Index = props => {
+  const {
+    dispatch,
+    form,
+    addStoryModalVisible,
+    itAssessModalVisible,
+    turnAssessModalVisible,
+    selectedStoryRows,
+    setSelectedStoryRows,
+    handleModalVisible,
+    handleQueryStoryList,
+    loadingQueryStoryData,
+    demandInfo,
+    demand: { storyList },
+    global: { systemList, userList },
+  } = props;
 
-  const { dispatch, form, addStoryModalVisible, itAssessModalVisible, turnAssessModalVisible,
-    selectedStoryRows, setSelectedStoryRows, handleModalVisible,
-    handleQueryStoryList, loadingQueryStoryData, demandInfo,
-    demand: { storyList }, global: { systemList, userList }} = props
-
-  const [searchMore, setSearchMore] = useState(false)
+  const [searchMore, setSearchMore] = useState(false);
+  const [addModalVisible, setAddModalVisible] = useState(false);
+  const [selectedRows, setSelectedRows] = useState({});
 
   const handleStoryTableChange = pagination => {
-      // const formValues = form.getFieldsValue();
-      const params = {
-        currentPage: pagination.current,
-        pageSize: pagination.pageSize,
-        // ...formValues, // 添加已查询条件去获取分页
-      };
-      handleQueryStoryList(params);
+    // const formValues = form.getFieldsValue();
+    const params = {
+      currentPage: pagination.current,
+      pageSize: pagination.pageSize,
+      // ...formValues, // 添加已查询条件去获取分页
     };
+    handleQueryStoryList(params);
+  };
 
   const handleQuerySystemList = () => {
     dispatch({
-      type: "global/querySystemList",
+      type: 'global/querySystemList',
       payload: {
         ...PagerHelper.MaxPage,
-      }
-    })
-  }
+      },
+    });
+  };
 
   const handleQueryUserList = () => {
     dispatch({
-      type: "global/queryUserList",
+      type: 'global/queryUserList',
       payload: {
         ...PagerHelper.MaxPage,
-      }
-    })
-  }
+      },
+    });
+  };
+
+  const handleUpdateStory = ids => {
+    dispatch({
+      type: 'demand/updateStory',
+      payload: {
+        id: ids,
+        isDelete: 1,
+      },
+    }).then(sure => {
+      if (!sure) return;
+      message.success('删除成功');
+      handleQueryStoryList();
+    });
+  };
+
+  // 同步JIRA
+  const handleSyncStory = () => {
+    dispatch({
+      type: 'demand/syncStory',
+      payload: {
+        storyId: props?.location?.query?.id,
+      },
+    }).then(res => {
+      if (!res) return;
+      message.success('同步成功');
+      handleQueryStoryList();
+    });
+  };
 
   useEffect(() => {
-    handleQuerySystemList()
-    handleQueryUserList()
-  }, [])
+    handleQuerySystemList();
+    handleQueryUserList();
+  }, []);
   const storyColumns = [
-    TableColumnHelper.genPopoverColumn('number', 'story编号', 8),
+    {
+      title: 'story编号',
+      key: 'number',
+      sorter: true,
+      width: 120,
+      render: rows => {
+        if (isEmpty(rows.number, true)) return '';
+        return (
+          <Tooltip placement="top" title={rows.number}>
+            <span
+              style={{ color: '#2E5BFF', cursor: 'pointer' }}
+              onClick={() => {
+                router.push({
+                  pathname: '/demand/storyDetail',
+                  query: {
+                    id: rows.id,
+                  },
+                });
+              }}
+            >
+              {rows.number.length > 8
+                ? `${rows.number.substring(0, 8)}...`
+                : rows.number.substring(0, 8)}
+            </span>
+          </Tooltip>
+        );
+      },
+    },
     TableColumnHelper.genPlanColumn('title', '标题'),
     TableColumnHelper.genPlanColumn('status', '状态'),
     TableColumnHelper.genPlanColumn('priority', '优先级'),
     TableColumnHelper.genPlanColumn('type', 'story类型'),
     TableColumnHelper.genPlanColumn('systemName', '所属系统'),
-    TableColumnHelper.genDateTimeColumn('evaluateTime', 'IT预计上线时间', "YYYY-MM-DD"),
+    TableColumnHelper.genDateTimeColumn('evaluateTime', 'IT预计上线时间', 'YYYY-MM-DD'),
     TableColumnHelper.genPlanColumn('developWorkload', '开发预计测试工作量'),
     TableColumnHelper.genPlanColumn('testWorkload', '测试预计测试工作量'),
     TableColumnHelper.genPlanColumn('assessor', '评估人'),
     TableColumnHelper.genPlanColumn('userName', '创建人'),
     TableColumnHelper.genDateTimeColumn('createTime', '创建时间'),
+    {
+      title: '操作',
+      width: 170,
+      align: 'center',
+      render: rows => {
+        const { userInfo } = storage.get('gd-user', {});
+        const isDelete = userInfo?.userId === rows?.userId && !rows?.issueId;
+        return (
+          <Fragment>
+            <OptButton
+              img={edit}
+              showText={false}
+              text="编辑"
+              onClick={() => {
+                setAddModalVisible(true);
+                setSelectedRows(rows);
+              }}
+            />
+            <Divider type="vertical" />
+            <OptButton
+              icon="eye"
+              text="查看"
+              showText={false}
+              onClick={() => {
+                router.push({
+                  pathname: '/demand/storyDetail',
+                  query: {
+                    id: rows.id,
+                  },
+                });
+              }}
+            />
+            <Divider type="vertical" />
+
+            <Popconfirm
+              title="确定要同步JIRA吗?"
+              onConfirm={(userInfo.userId !== rows.assessor || rows?.issueId) && handleSyncStory}
+              okText="确定"
+              cancelText="取消"
+            >
+              <OptButton
+                icon="sync"
+                text="同步"
+                disabled={userInfo.userId !== rows.assessor || rows?.issueId}
+                showText={false}
+              />
+            </Popconfirm>
+            <Divider type="vertical" />
+
+            <Popconfirm
+              title={`确定要删除${rows.title}吗?`}
+              onConfirm={() => isDelete && handleUpdateStory(rows.id)}
+              okText="确定"
+              cancelText="取消"
+            >
+              <OptButton
+                img={deleteIcon}
+                showText={false}
+                style={isDelete ? { color: '#d63649' } : { color: '#b0bac9' }}
+                text="删除"
+                disabled={!isDelete}
+              />
+            </Popconfirm>
+          </Fragment>
+        );
+      },
+    },
   ];
 
-  const handleSearchForm = ()=> {
+  const handleSearchForm = () => {
     const formValues = form.getFieldsValue();
-    const params =  {
-      startTime: formValues?.RangeDate ? formValues?.RangeDate[0].format("YYYY-MM-DD") : null,
-      endTime: formValues?.RangeDate ? formValues?.RangeDate[1].format("YYYY-MM-DD") : null,
-    }
+    const params = {
+      startTime: formValues?.RangeDate ? formValues?.RangeDate[0].format('YYYY-MM-DD') : null,
+      endTime: formValues?.RangeDate ? formValues?.RangeDate[1].format('YYYY-MM-DD') : null,
+    };
     handleQueryStoryList({ ...formValues, ...params });
   };
 
@@ -86,7 +240,7 @@ const Index = props => {
     form.resetFields();
     setSearchMore(false);
     handleQueryStoryList();
-  }
+  };
   const renderStoryForm = () => {
     const { getFieldDecorator } = form;
     const content = (
@@ -94,11 +248,7 @@ const Index = props => {
         <Row>
           <Col span={24}>
             <FormItem {...formLayoutItem2} label="创建时间">
-              {getFieldDecorator('RangeDate')(
-                <RangePicker
-                  format="YYYY-MM-DD"
-                />
-              )}
+              {getFieldDecorator('RangeDate')(<RangePicker format="YYYY-MM-DD" />)}
             </FormItem>
           </Col>
           <Col span={24}>
@@ -110,7 +260,7 @@ const Index = props => {
                       {v.value}
                     </Option>
                   ))}
-                </Select>
+                </Select>,
               )}
             </FormItem>
           </Col>
@@ -118,19 +268,22 @@ const Index = props => {
             <FormItem {...formLayoutItem2} label="评估人">
               {getFieldDecorator('assessor')(
                 <Select placeholder="请选择评估人" allowClear>
-                  {userList?.list && userList.list.map(v => (
-                    <Option value={v.loginid} key={v.loginid.toString()}>
-                      {v.lastname}
-                    </Option>
-                  ))}
-                </Select>
+                  {userList?.list &&
+                    userList.list.map(v => (
+                      <Option value={v.loginid} key={v.loginid.toString()}>
+                        {v.lastname}
+                      </Option>
+                    ))}
+                </Select>,
               )}
             </FormItem>
           </Col>
         </Row>
         <div className={styles.moreSearchButton}>
           <Button onClick={() => setSearchMore(false)}>取消</Button>
-          <Button type="primary" onClick={handleSearchForm}>查询</Button>
+          <Button type="primary" onClick={handleSearchForm}>
+            查询
+          </Button>
         </div>
       </div>
     );
@@ -140,55 +293,47 @@ const Index = props => {
           <Col span={5}>
             <FormItem {...formLayoutItem} label="story编号标题" colon={false}>
               {getFieldDecorator('title')(
-                <Input
-                  allowClear
-                  onBlur={handleSearchForm}
-                  placeholder="请输入story编号标题"
-                />)}
+                <Input allowClear onBlur={handleSearchForm} placeholder="请输入story编号标题" />,
+              )}
             </FormItem>
           </Col>
           <Col span={5}>
             <FormItem {...formLayoutItem} label="状态" colon={false}>
               {getFieldDecorator('status')(
-                <Select
-                  placeholder="请选择story状态"
-                >
-                  {
-                    STORY_STATUS.map(v => (
-                      <Option value={v.value} key={v.value}>{v.value}</Option>
-                    ))
-                  }
-                </Select>
-                )}
+                <Select placeholder="请选择story状态">
+                  {STORY_STATUS.map(v => (
+                    <Option value={v.value} key={v.value}>
+                      {v.value}
+                    </Option>
+                  ))}
+                </Select>,
+              )}
             </FormItem>
           </Col>
           <Col span={5}>
             <FormItem {...formLayoutItem} label="优先级" colon={false}>
               {getFieldDecorator('priority')(
-                <Select
-                  placeholder="请选择优先级"
-                >
-                  {
-                    STORY_PRIORITY.map(v => (
-                      <Option value={v.key} key={v.key}>{v.value}</Option>
-                    ))
-                  }
-                </Select>
+                <Select placeholder="请选择优先级">
+                  {STORY_PRIORITY.map(v => (
+                    <Option value={v.key} key={v.key}>
+                      {v.value}
+                    </Option>
+                  ))}
+                </Select>,
               )}
             </FormItem>
           </Col>
           <Col span={5}>
             <FormItem {...formLayoutItem} label="所属系统" colon={false}>
               {getFieldDecorator('systemId')(
-                <Select
-                  placeholder="请选择所属系统"
-                >
-                  {
-                    systemList?.list && systemList.list.map(v => (
-                      <Option value={v.id} key={v.id}>{v.name}</Option>
-                    ))
-                  }
-                </Select>
+                <Select placeholder="请选择所属系统">
+                  {systemList?.list &&
+                    systemList.list.map(v => (
+                      <Option value={v.id} key={v.id}>
+                        {v.name}
+                      </Option>
+                    ))}
+                </Select>,
               )}
             </FormItem>
           </Col>
@@ -215,19 +360,17 @@ const Index = props => {
                 trigger="click"
               >
                 <div className="yCenter">
-                  {
-                    !searchMore ? (
-                      <span className="activeColor" onClick={() => setSearchMore(true)}>
-                        <Icon style={{ verticalAlign: '-0.4em' }} component={bottomIcon} />
-                        更多
-                      </span>
-                    ) : (
-                      <span className="activeColor" onClick={() => setSearchMore(false)}>
-                        <Icon style={{ verticalAlign: '-0.4em' }} component={upIcon} />
-                        隐藏
-                      </span>
-                    )
-                  }
+                  {!searchMore ? (
+                    <span className="activeColor" onClick={() => setSearchMore(true)}>
+                      <Icon style={{ verticalAlign: '-0.4em' }} component={bottomIcon} />
+                      更多
+                    </span>
+                  ) : (
+                    <span className="activeColor" onClick={() => setSearchMore(false)}>
+                      <Icon style={{ verticalAlign: '-0.4em' }} component={upIcon} />
+                      隐藏
+                    </span>
+                  )}
                 </div>
               </Popover>
             </div>
@@ -253,31 +396,41 @@ const Index = props => {
         <AddStory
           type="add"
           modalVisible={addStoryModalVisible}
-          handleModalVisible={() => handleModalVisible(false, "addStoryModalVisible")}
-          values={{demandName: demandInfo?.title, demandNumber: demandInfo?.demandNumber}}
+          handleModalVisible={() => handleModalVisible(false, 'addStoryModalVisible')}
+          values={{ demandName: demandInfo?.title, demandNumber: demandInfo?.demandNumber }}
           handleQueryStoryList={handleQueryStoryList}
         />
       )}
       {itAssessModalVisible && (
         <ITAssess
           modalVisible={itAssessModalVisible}
-          handleModalVisible={() => handleModalVisible(false, "itAssessModalVisible")}
+          handleModalVisible={() => handleModalVisible(false, 'itAssessModalVisible')}
           values={demandInfo}
         />
       )}
       {turnAssessModalVisible && (
         <TurnAssess
           modalVisible={turnAssessModalVisible}
-          handleModalVisible={() => handleModalVisible(false, "turnAssessModalVisible")}
+          handleModalVisible={() => handleModalVisible(false, 'turnAssessModalVisible')}
           values={demandInfo}
         />
       )}
+      {addModalVisible && (
+        <AddStory
+          values={selectedRows}
+          modalVisible={addModalVisible}
+          handleModalVisible={() => {
+            setAddModalVisible(false);
+            setSelectedRows({});
+          }}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default connect(({ global, demand, loading }) => ({
   global,
   demand,
   loadingQueryStoryData: loading.effects['demand/queryStoryList'],
-}))(Form.create()(Index))
+}))(Form.create()(Index));
